b7d67cd0cb1f962798dceb4b1dd36515149af108
pistachionet <navid@Navids-MacBook-Pro.local>
2022-11-24 08:34:12 -0500

    added the retreive method needs review

--- Blossom.py
+++ Blossom.py
@@ -11,9 +11,16 @@ class HashMap:
         return hash_code % self.array_size 
 
     def assign(self, key, value):
-        array_index = self.compress(hash(key))
+        array_index = self.compress(self.hash(key))
         self.array[array_index] = [key, value]
 
-        
+    def retrieve(self, key):
+        array_index = self.compress((self.hash(key)))
+        payload = self.array[array_index]
+        if payload != None:
+            if payload[0] == key:
+                return payload[1]
+        elif payload == None or payload[0] != key:
+            return None
 
 
