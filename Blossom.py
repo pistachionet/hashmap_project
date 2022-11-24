c0c506b05b4672861ceeacc12b75c72bb24d777e
Navid Misaghian <navid@Navids-MacBook-Pro.local>
2022-11-24 08:06:02 -0500

    made class, compess method, and hash function method

---
+++ Blossom.py
@@ -0,0 +1,14 @@
+class HashMap:
+    def __init__(self, size) -> None:
+        self.array_size = size
+        self.array = [None] * size #list of none objects of length size 
+
+
+    def hash(self, key):
+       return sum(key.encode())
+
+    def compress(self, hash_code):
+        return hash_code % self.array_size 
+
+
+
