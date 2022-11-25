50896260a6499a56999339afd0268a44269ec2cb
pistachionet <misa@navid.buzz>
2022-11-24 20:24:04 -0500

    created instaces

--- Blossom.py
+++ Blossom.py
@@ -1,19 +1,20 @@
 from linkedlist import Node, LinkedList
+from flower_definitions import flower_definitions
+
 class HashMap:
     def __init__(self, size) -> None:
         self.array_size = size
-        self.array = [LinkedList() for i in range(self.array_size)] #list of none objects of length size 
-
+        self.array = [LinkedList() for i in range(self.array_size)]  # list of none objects of length size
 
     def hash(self, key):
-       return sum(key.encode())
+        return sum(key.encode())
 
     def compress(self, hash_code):
-        return hash_code % self.array_size 
+        return hash_code % self.array_size
 
     def assign(self, key, value):
         array_index = self.compress(self.hash(key))
-        payload = Node([key,value])
+        payload = Node([key, value])
         list_at_array = self.array[array_index]
         for i in list_at_array:
             if i[0] == key:
@@ -27,3 +28,9 @@ class HashMap:
             if i[0] == key:
                 return i[1]
         return None
+
+
+
+blossom = HashMap(len(flower_definitions))
+for flower in flower_definitions:
+    blossom.assign(flower[0],flower[1])
